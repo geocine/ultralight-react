07d1ff0336afdcf41b674741159368be1386b29a
Aivan Monceller <aivan@monceller.com>
2021-02-12 23:02:57 +0800

    chore(init): initial commit

---
+++ README.md
@@ -0,0 +1,92 @@
+<h1 align="center">
+  <br>
+  <a href="https://github.com/geocine/ulralight-react#readme"><img src="https://i.imgur.com/G4Szs99.png" alt="header" width="600"/></a>
+  <br>
+  Ultralight React Starter
+  <br>
+</h1>
+
+Build desktop applications using a smaller, lighter, cross-platform WebKit that's more customizable and designed to display web technologies like React. This is an [Ultralight](https://github.com/ultralight-ux/Ultralight) starter with [React](https://github.com/facebook/react). 
+
+
+<p align="center">
+  <img src="https://i.imgur.com/3QF2ZPv.png" alt="ultralight" width="600"/>
+</p>
+
+## Available Commands
+
+### React
+Commands you can execute inside the [React](https://github.com/facebook/react) `app` directory
+
+| Command | Description |
+|---------|-------------|
+| `yarn install` | Install project dependencies |
+| `yarn build` | Builds code bundle with production settings  |
+| `yarn start` | Run a web server to serve built code bundle |
+
+### Ultralight
+Commands you can execute inside the build 
+
+| Command | Description |
+|---------|-------------|
+| `cmake --build . --config Release` | Build Ultralight binary without cleaning |
+| `cmake --build . --config Release --clean-first` | Build Ultralight binary |
+
+## Prerequisites
+
+Before you build and run, you'll need to [install the Ultralight prerequisites](https://docs.ultralig.ht/docs/installing-prerequisites) for your platform.
+
+You also need to [install NodeJS](https://nodejs.org/en/download/) to work with React
+## Development
+
+### React
+
+Inside the `app` folder resides React, you need to install dependencies by executing the following commands
+
+```shell
+yarn
+```
+
+### Ultralight
+
+Setup the Ultralight build folder, you only have to do this once by executing the following commands
+
+```shell
+mkdir build
+cd build
+cmake ..
+cmake --build . --config Release
+```
+
+Once the above is setup everytime you need to rebuild the application you need to execute the following on the `build` directory
+
+```shell
+cmake --build . --config Release --clean-first
+```
+
+> Note you may need to delete the binary eg. `MyApp` in this case.
+
+
+## Running
+
+### Ultralight 
+#### On macOS and Linux
+
+Navigate to `ultralight-quick-start/build` and run `MyApp` to launch the program.
+
+#### On Windows
+
+Navigate to `ultralight-quick-start/build/Release` and run `MyApp` to launch the program.
+
+### React
+
+To run your React application and debug, just go to `app` folder and execute the
+```shell
+yarn start
+```
+
+## Further Reading
+
+Follow the [Writing Your First Ultralight App](https://docs.ultralig.ht/docs/writing-your-first-app) guide and other tutorials in the documentation for more info.
+
+The React application is bootstraped with [Create React App](https://create-react-app.dev/)
---
+++ src/MyApp.cpp
@@ -0,0 +1,129 @@
+#include "MyApp.h"
+
+#define WINDOW_WIDTH  600
+#define WINDOW_HEIGHT 400
+
+MyApp::MyApp() {  
+  app_ = App::Create();
+
+  ///
+  /// Create a resizable window by passing by OR'ing our window flags with
+  /// kWindowFlags_Resizable.
+  ///
+  window_ = Window::Create(app_->main_monitor(), WINDOW_WIDTH, WINDOW_HEIGHT,
+    false, kWindowFlags_Titled | kWindowFlags_Resizable);
+
+  ///
+  /// Tell our app to use 'window' as our main window.
+  ///
+  /// This call is required before creating any overlays or calling App::Run
+  ///
+  app_->set_window(*window_.get());
+
+  ///
+  /// Create our HTML overlay-- we don't care about its initial size and
+  /// position because it'll be calculated when we call OnResize() below.
+  ///
+  overlay_ = Overlay::Create(*window_.get(), 1, 1, 0, 0);
+
+  ///
+  /// Force a call to OnResize to perform size/layout of our overlay.
+  ///
+  OnResize(window_->width(), window_->height());
+
+  ///
+  /// Load a page into our overlay's View
+  ///
+  overlay_->view()->LoadURL("file:///index.html");
+
+  ///
+  /// Register our MyApp instance as an AppListener so we can handle the
+  /// App's OnUpdate event below.
+  ///
+  app_->set_listener(this);
+
+  ///
+  /// Register our MyApp instance as a WindowListener so we can handle the
+  /// Window's OnResize event below.
+  ///
+  window_->set_listener(this);
+
+  ///
+  /// Register our MyApp instance as a LoadListener so we can handle the
+  /// View's OnFinishLoading and OnDOMReady events below.
+  ///
+  overlay_->view()->set_load_listener(this);
+
+  ///
+  /// Register our MyApp instance as a ViewListener so we can handle the
+  /// View's OnChangeCursor and OnChangeTitle events below.
+  ///
+  overlay_->view()->set_view_listener(this);
+}
+
+MyApp::~MyApp() {
+}
+
+void MyApp::Run() {
+  app_->Run();
+}
+
+void MyApp::OnUpdate() {
+  ///
+  /// This is called repeatedly from the application's update loop.
+  ///
+  /// You should update any app logic here.
+  ///
+}
+
+void MyApp::OnClose() {
+}
+
+void MyApp::OnResize(uint32_t width, uint32_t height) {
+  ///
+  /// This is called whenever the window changes size (values in pixels).
+  ///
+  /// We resize our overlay here to take up the entire window.
+  ///
+  overlay_->Resize(width, height);
+}
+
+void MyApp::OnFinishLoading(ultralight::View* caller,
+                            uint64_t frame_id,
+                            bool is_main_frame,
+                            const String& url) {
+  ///
+  /// This is called when a frame finishes loading on the page.
+  ///
+}
+
+void MyApp::OnDOMReady(ultralight::View* caller,
+                       uint64_t frame_id,
+                       bool is_main_frame,
+                       const String& url) {
+  ///
+  /// This is called when a frame's DOM has finished loading on the page.
+  ///
+  /// This is the best time to setup any JavaScript bindings.
+  ///
+}
+
+void MyApp::OnChangeCursor(ultralight::View* caller,
+                           Cursor cursor) {
+  ///
+  /// This is called whenever the page requests to change the cursor.
+  ///
+  /// We update the main window's cursor here.
+  ///
+  window_->SetCursor(cursor);
+}
+
+void MyApp::OnChangeTitle(ultralight::View* caller,
+                          const String& title) {
+  ///
+  /// This is called whenever the page requests to change the title.
+  ///
+  /// We update the main window's title here.
+  ///
+  window_->SetTitle(title.utf8().data());
+}
